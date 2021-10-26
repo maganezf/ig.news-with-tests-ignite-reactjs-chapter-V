cf0edc54b20951ea447c05890b76eb00c2da36f4
maganezf <maganezfilho@gmail.com>
2021-10-26 12:27:18 -0300

    feat(activelink.spec): added ActiveLink test
    n

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,7 +1,6 @@
-import { ReactElement, cloneElement } from 'react';
 import Link, { LinkProps } from 'next/link';
-
 import { useRouter } from 'next/router';
+import { cloneElement, ReactElement } from 'react';
 
 interface ActiveLinkProps extends LinkProps {
   activeClassName: string;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,21 @@
 import Image from 'next/image';
-
 import logoImg from '../../../public/logo.svg';
-
 import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
-
 import styles from './styles.module.scss';
 
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src={logoImg} alt="ig.news" />
+        <Image src={logoImg} alt='ig.news' />
 
         <nav>
-          <ActiveLink activeClassName={styles.active} href="/">
+          <ActiveLink activeClassName={styles.active} href='/'>
             <a>Home</a>
           </ActiveLink>
 
-          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
             <a>Posts</a>
           </ActiveLink>
         </nav>
